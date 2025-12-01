9fe595864447dedb7e2755b59fd4b2a76d39e00f
Amro145 <amroaltayeb14@gmail.com>
2025-12-01 23:50:16 +0200

    logout2

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,30 +1,35 @@
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
-const dispatch = useDispatch();
+
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
 
-axiosInstance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (error.response && error.response.status === 500) {
-            // Redirect to the server error page
-            window.location.href = '/server-error';
+// This function allows configuring the interceptors with Redux dispatch
+// It should be called from a React component or a setup function where useDispatch can be called.
+export const setupAxiosInterceptors = (dispatch, logoutAction) => {
+    axiosInstance.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+            if (error.response && error.response.status === 500) {
+                // Redirect to the server error page
+                window.location.href = '/server-error';
+            }
+            if (error.response && error.response.status === 401) {
+                // Redirect to the server error page
+                // logout
+                if (dispatch && logoutAction) {
+                    dispatch(logoutAction());
+                }
+                localStorage.removeItem('userData');
+                localStorage.removeItem('token');
+                window.location.href = '/login';
+            }
+            return Promise.reject(error);
         }
-        if (error.response && error.response.status === 401) {
-            // Redirect to the server error page
-            // logout
-            dispatch(logout());
-            localStorage.removeItem('userData');
-            localStorage.removeItem('token');
-            window.location.href = '/login';
-        }
-        return Promise.reject(error);
-    }
-);
+    );
+};
 
 export default axiosInstance;
