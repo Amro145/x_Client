330294c7abd3f61af8e48c79d78d2960671bf0b0
Amro145 <amroaltayeb14@gmail.com>
2026-02-11 02:27:42 +0200

    Add logout button to mobile navigation

--- src/components/Home/MobileNav.jsx
+++ src/components/Home/MobileNav.jsx
@@ -3,16 +3,19 @@ import { MdHomeFilled } from "react-icons/md";
 import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { BiLogOut } from "react-icons/bi";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../../store/api/authApi";
 
 const MobileNav = () => {
     const { userData } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
     const location = useLocation();
 
     const isActive = (path) => location.pathname === path;
 
     return (
-        <div className="md:hidden fixed bottom-0 left-0 right-0 bg-black border-t border-gray-800 flex justify-around items-center py-3 z-50 backdrop-blur-md bg-black/90">
+        <div className="md:hidden fixed bottom-0 left-0 right-0 bg-black border-t border-gray-800 flex justify-around items-center py-3 z-50 backdrop-blur-md bg-black/90 px-4">
             <Link to="/" className="p-2 transition-colors">
                 <MdHomeFilled
                     size={28}
@@ -31,6 +34,12 @@ const MobileNav = () => {
                     className={isActive(`/profile/${userData?._id}`) ? "text-white" : "text-gray-500"}
                 />
             </Link>
+            <button
+                onClick={() => dispatch(logout())}
+                className="p-2 text-gray-500 hover:text-red-500 transition-colors cursor-pointer"
+            >
+                <BiLogOut size={28} />
+            </button>
         </div>
     );
 };
